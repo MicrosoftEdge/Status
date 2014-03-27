860c41f9ea205436cbbb6c79d730319b3c118e0d
molant <antonmolleda@hotmail.com>
2014-03-26 17:53:21 -0700

    Implementing the correct behavior of browser support filter

--- app/scripts/directives/filter.js
+++ app/scripts/directives/filter.js
@@ -29,15 +29,46 @@ angular.module('statusieApp')
                             acum.push(item);
                         } else if (_.contains(selectedOptions, item[property])) {
                             acum.push(item);
+                        }else if(typeof property === 'function'){
+                            property(selectedOptions, acum, item);
                         }
 
                         return acum;
                     };
                 };
 
+                var browserFilter = function(selectedBrowser, acum, item){
+                    var add = true;
+
+                    if(!selectedBrowser){
+                        return;
+                    }
+
+                    selectedBrowser = _.map(selectedBrowser, function(option){
+                        var optionLowerCase =option.toLowerCase();
+
+                        if(optionLowerCase === 'internet explorer'){
+                            optionLowerCase = 'ie';
+                        }
+
+                        return optionLowerCase;
+                    });
+
+                    var itemBrowsers = item.browsers;
+
+                    _.forEach(selectedBrowser, function(browser){
+                        if(!itemBrowsers[browser].status){
+                            add = false;
+                        }
+                    });
+                    if(add){
+                        acum.push(item);
+                    }
+                };
+
                 $scope.filters = {
                     areas: [selectorFilter('categories', 'category')],
-                    browsers: [selectorFilter('browsers', 'category')],
+                    browsers: [selectorFilter('browsers', browserFilter)],
                     status: [selectorFilter('featureStatus', 'position')]
                 };
             }
