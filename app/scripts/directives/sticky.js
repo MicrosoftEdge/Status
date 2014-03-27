ece43d553aa0938499463ebeda22f9edfd760cc7
molant <antonmolleda@hotmail.com>
2014-03-26 21:09:16 -0700

    Solving the top margin issues

--- app/scripts/directives/sticky.js
+++ app/scripts/directives/sticky.js
@@ -9,14 +9,19 @@ angular.module('statusieApp')
                 var $win = angular.element($window);
                 var locked = false;
 
+                var fill = $('<div class="hide"></div>').insertBefore(element);
+
                 var process = function () {
                     var needsLock = $win.scrollTop() >= topOffset;
                     if (needsLock !== locked) {
                         locked = needsLock;
                         if (locked) {
+                            fill.height(element.height());
                             element.addClass('fixed');
+                            fill.removeClass('hide');
                         } else {
                             element.removeClass('fixed');
+                            fill.addClass('hide');
                         }
                     }
                 };
