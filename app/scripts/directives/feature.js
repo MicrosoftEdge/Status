14876804d4f765d10215b8a002b122c3f3e3609e
molant <antonmolleda@hotmail.com>
2014-03-30 18:47:58 -0700

    Giving more space to the header when <500px width

--- app/scripts/directives/feature.js
+++ app/scripts/directives/feature.js
@@ -7,9 +7,9 @@ angular.module('statusieApp')
             restrict: 'E',
             replace: true,
             link: function (scope, element, attrs) {
-                var featureName = $(element).find('h1.feature_name');
-                var featureInfoWidth = $(element).find('.feature-info').width();
-                featureName.css('max-width', 'calc(100% - ' + (featureInfoWidth + 10) + 'px)');
+//                var featureName = $(element).find('h1.feature_name');
+//                var featureInfoWidth = $(element).find('.feature-info').width();
+//                featureName.css('max-width', 'calc(100% - ' + (featureInfoWidth + 10) + 'px)');
             }
         };
     });
