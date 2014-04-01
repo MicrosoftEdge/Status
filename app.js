801cf8a5ae077865d4694357ac68033309474313
molant <antonmolleda@hotmail.com>
2014-03-31 18:18:08 -0700

    Adding CORS

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var express = require('express'),
     cors = require('cors'),
     path = require('path'),
+    fs = require('fs'),
     port = process.env.PORT || 9000,
     app = express();
 
+var content = fs.readFileSync(path.join('app', 'static', 'ie-status.json'), {encoding: 'utf8'});
+
 app.use(express.compress());
-//We shouldn't need to add the options as we only allow the GET verb
-app.use(cors());
+app.options('/features', cors());
+app.get('/features', cors(), function(req, res, next){
+    res.writeHeader(200, {"Content-Type": "application/json"});
+    res.write(content);
+    res.end();
+});
 app.use(express.basicAuth('admin','IE11Rocks!'));
 app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, 'app')));
 
 app.listen(port);
+
--- app/scripts/services/status.js
+++ app/scripts/services/status.js
@@ -5,7 +5,7 @@ angular.module('statusieApp')
         var chromeStatusURL = 'http://www.chromestatus.com/features.json';
 
         //TODO: load this from a remote url
-        var ieStatusURL = '/static/ie-status.json';
+        var ieStatusURL = '/features';
         var observedBrowsers = _.map(['Internet Explorer', 'Chrome', 'Firefox', 'Safari', 'Opera'], function (browser) {
             return {name: browser, selected: false};
         });
