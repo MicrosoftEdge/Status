dc8e02106a99ee4e294452a63764dd43f3ed352d
molant <antonmolleda@hotmail.com>
2014-03-31 18:10:28 -0700

    Adding CORS support

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express'),
+    cors = require('cors'),
     path = require('path'),
     port = process.env.PORT || 9000,
     app = express();
 
 app.use(express.compress());
+//We shouldn't need to add the options as we only allow the GET verb
+app.use(cors());
 app.use(express.basicAuth('admin','IE11Rocks!'));
 app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, 'app')));
+
 app.listen(port);
