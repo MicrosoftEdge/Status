b4431ebd5e5b50990c34e44b1c96d251b2ff6568
molant <amolleda@gmail.com>
2014-04-03 16:15:38 -0700

    semantic-url working!

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,10 @@
 angular.module('statusieApp', [
     'ngCookies',
     'ngSanitize',
-    'ngRoute',
     'pasvaz.bindonce',
     'ui.bootstrap'
 ])
-    .config(function ($routeProvider, $locationProvider) {
+    .config(function ($locationProvider) {
         'use strict';
-        $routeProvider
-            .when('/:id', {
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl'
-            });
-
         $locationProvider.html5Mode(true);
     });
