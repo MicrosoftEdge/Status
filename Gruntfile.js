dd9b7a17d9d5e982357b8add60d878caf12241b3
molant <antonmolleda@hotmail.com>
2014-04-14 13:09:19 -0700

    Playing with grunt-contrib-imagemin options

--- Gruntfile.js
+++ Gruntfile.js
@@ -189,6 +189,7 @@ module.exports = function (grunt) {
         imagemin: {
             dist: {
                 options: {
+                    optimizationLevel: 0,
                     pngquant: true
                 },
                 files: [
