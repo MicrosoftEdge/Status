f0f46dd670461f392c00c544170e72fa5ad5c4ec
molant <antonmolleda@hotmail.com>
2014-05-14 11:24:34 -0700

    some tests

--- app/scripts/directives/feature.js
+++ app/scripts/directives/feature.js
@@ -10,18 +10,6 @@ angular.module('statusieApp')
                     $scope.show = false;
                 });
 
-                $scope.expand = function ($event) {
-                    $scope.show = !$scope.show;
-                    if ($scope.show) {
-                        $location.path('/' + $scope.feature.normalized_name);
-                        $event.target.nextElementSibling.style.display = "block";
-                        $event.target.parentNode.style.height = ($event.target.nextElementSibling.offsetHeight + $event.target.offsetHeight) + "px";
-                    } else {
-                        $event.target.nextElementSibling.style.display = "none";
-                        $event.target.parentNode.style.height = $event.target.offsetHeight + "px";
-                    }
-                };
-
                 var id = $location.path();
                 if (id === '/' + $scope.feature.normalized_name) {
                     $scope.show = true;
@@ -31,9 +19,11 @@ angular.module('statusieApp')
                 //The following code is for accessibility
                 //We do it by code instead of binds because it will add lots of watchers and we will be over the
                 //recommended number
-                var header = element[0].querySelector('button');
-                var featureWrapper = element[0].querySelector('.feature-body-wrapper');
-                scope.$watch('show', function(newValue, oldValue){
+                var feature = element[0];
+                var header = feature.querySelector('button');
+                var headerHeight = header.offsetHeight;
+                var featureWrapper = feature.querySelector('.feature-body-wrapper');
+                scope.$watch('show', function(newValue){
                     if(newValue){
                         header.setAttribute('aria-expanded','true');
                         featureWrapper.setAttribute('aria-hidden','false');
@@ -42,6 +32,18 @@ angular.module('statusieApp')
                         featureWrapper.setAttribute('aria-hidden','true');
                     }
                 });
+
+                scope.expand = function () {
+                    scope.show = !scope.show;
+                    if (scope.show) {
+                        featureWrapper.style.display = "block";
+                        element.style.height = (featureWrapper.offsetHeight + 30) + "px";
+                        featureWrapper.style.height = "auto";
+                    } else {
+                        featureWrapper.style.display = "none";
+                        element.style.height = headerHeight + "px";
+                    }
+                };
 
                 header.addEventListener('focus', function(){
                     header.setAttribute('aria-selected', 'true');
