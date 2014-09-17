4ee273a84d24c8b4edfd9a883b5aebe9b6fd6ddb
Anton Molleda <amolleda@gmail.com>
2014-09-16 19:40:10 -0700

    Ignoring npm errors during deployment

--- deploy.cmd
+++ deploy.cmd
@@ -124,8 +124,9 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install
-  IF !ERRORLEVEL! NEQ 0 goto error
+  call :ExecuteCmd !NPM_CMD! install --silent
+  :: we are going to have an error with phantonmjs so we ignore this
+  :: IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
 
